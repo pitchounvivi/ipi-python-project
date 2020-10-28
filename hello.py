b312a4ccc6c822ada12550c29753691470335c5c
pitchounvivi <pitchounvivi@gmail.com>
2020-10-28 23:31:48 +0100

    basic project architecture

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python3 #ligne pour l'interpréteur Python
+
+# coding: utf-8
+
 """Module Flask"""
 #the compliance checker with pep8 (pylint) requires me to have a comment before imports
 from flask import Flask
@@ -8,5 +12,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    """display test"""
-    return 'Hello, World! ici'
+    """display Hello, World for test"""
+    return 'Hello, World! ici aussi'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
