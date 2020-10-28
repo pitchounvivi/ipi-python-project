6f6ede3b773ebbd874c29e8583aa7d92db9806f4
pitchounvivi <pitchounvivi@gmail.com>
2020-10-28 23:47:51 +0100

    routing to index.html

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-#file containing routes
+#! /usr/bin/env python3 #ligne pour l'interpréteur Python
+# coding: utf-8
+
+"""Flask"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+"""routing"""
+@app.route('/')
+
+def index():
+    """homepage"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
