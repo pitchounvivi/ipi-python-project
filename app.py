7b60665fe2fcf65b113b8a43014adb807e1b43d8
pitchounvivi <pitchounvivi@gmail.com>
2020-10-30 23:45:49 +0100

    link to style.css and config.py

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-"""routing"""
-@app.route('/')
+# Config options
+app.config.from_object('config')
 
+@app.route('/')
 def index():
     """homepage"""
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    """login"""
+    return render_template('login')
 
 if __name__ == "__main__":
     app.run(debug=True)
