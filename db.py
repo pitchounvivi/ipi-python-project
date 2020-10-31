c0a042efdc890f14438d65d969176574317b480c
Cristobal31 <boriosc@gmail.com>
2020-10-31 10:23:06 +0100

    feature: db initialiazation

--- db.py
+++ db.py
@@ -4,7 +4,8 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-def get_db():
+def get_db() :
+    """Open the database"""
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -14,8 +15,25 @@ def get_db():
 
     return g.db
 
-def close_db(e=None):
+def close_db(e=None) :
+    """Close the database"""
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
+def init_db() :
+    db = get_db()
+
+    with current_app.open_ressource('lvdeh.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app) :
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
