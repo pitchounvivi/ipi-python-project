39bff42d2e7e0c6eec32f969f548382e15dcde48
Cristobal31 <boriosc@gmail.com>
2020-11-02 00:35:19 +0100

    change: form and sql request

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -38,7 +38,7 @@ def register():
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, lastname, firstname, email,password) VALUES (?, ?, ?, ?, ?)',
+                'INSERT INTO user (user_username, user_lastname, user_firstname, user_email, user_password) VALUES (?, ?, ?, ?, ?)',
                 (username, lastname, firstname, email, generate_password_hash(password))
             )
             db.commit()
