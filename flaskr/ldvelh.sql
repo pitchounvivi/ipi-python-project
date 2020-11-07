0d3bfe6b9724eab90355da00a8a1b8b648447590
pitchounvivi <pitchounvivi@gmail.com>
2020-11-07 18:24:28 +0100

    filling table book test

--- flaskr/ldvelh.sql
+++ flaskr/ldvelh.sql
@@ -6,8 +6,12 @@
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
+------------------------------------------------------------------
+-- Creation area of the different tables
+------------------------------------------------------------------
+
 -- Table : book
--- DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS book;
 
 CREATE TABLE book (
     book_id    INTEGER       PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +20,7 @@ CREATE TABLE book (
 
 
 -- Table : book_user
--- DROP TABLE IF EXISTS book_user;
+DROP TABLE IF EXISTS book_user;
 
 CREATE TABLE book_user (
     book_id   INTEGER (11) REFERENCES book (book_id),
@@ -26,7 +30,7 @@ CREATE TABLE book_user (
 
 
 -- Table : chapter
--- DROP TABLE IF EXISTS chapter;
+DROP TABLE IF EXISTS chapter;
 
 CREATE TABLE chapter (
     chap_id      INTEGER       PRIMARY KEY AUTOINCREMENT,
@@ -37,7 +41,7 @@ CREATE TABLE chapter (
 
 
 -- Table : lecture
--- DROP TABLE IF EXISTS lecture;
+DROP TABLE IF EXISTS lecture;
 
 CREATE TABLE lecture (
     id_user INTEGER REFERENCES user (user_id),
@@ -46,16 +50,37 @@ CREATE TABLE lecture (
 
 
 -- Table : user
--- DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
     user_id        INTEGER       PRIMARY KEY AUTOINCREMENT,
-    user_username  VARCHAR (50)  NOT NULL UNIQUE,
+    user_username  VARCHAR (50)  UNIQUE NOT NULL,
     user_firstname VARCHAR (50)  NOT NULL,
     user_lastname  VARCHAR (50)  NOT NULL,
     user_email     VARCHAR (150) NOT NULL,
     user_password  VARCHAR (20)  NOT NULL
 );
+
+
+------------------------------------------------------------------
+-- Area for filling tables for the site
+------------------------------------------------------------------
+
+-- Table : book
+INSERT INTO book (book_title)
+ VALUES
+ ('Promenade en forêt'),
+ ('Super Héros and Co'),
+ ('Bizarre vous avez dit bizarre')
+;
+
+
+INSERT INTO chapter (chap_title, chap_content, book_id)
+ VALUES
+ ('chap 1 : début'),
+ ('une super histoire'), 
+ (1)
+;
 
 
 COMMIT TRANSACTION;
