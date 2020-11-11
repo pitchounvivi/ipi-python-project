63ac06fe071445a2e8e9c9cd3ea38cc95eb5c348
pitchounvivi <pitchounvivi@gmail.com>
2020-11-11 17:23:31 +0100

    navigation between first page and page_other = ok

--- flaskr/book.py
+++ flaskr/book.py
@@ -40,5 +40,13 @@ def page(book_id):
 @bp.route('/book/<book_id>/page/<page_id>/')
 def page_other(book_id,page_id):
     """Display other pages"""
+    db = get_db()
+    book = db.execute(
+        'SELECT * FROM book WHERE book_id = ?',(book_id,)).fetchone()
 
-    return render_template('bookstore/page_other.html', book=book_id, chap=page_id)
+    chap = db.execute(
+        'SELECT * FROM book JOIN chapter WHERE chapter.book_id = book.book_id '
+        + ' AND chapter.chap_id = ?',(page_id,)).fetchone()
+    db.commit()
+
+    return render_template('bookstore/page_other.html', book=book, chap=chap)
